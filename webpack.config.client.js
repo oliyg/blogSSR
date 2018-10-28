9bf382ff61e6d4fd64707fcebec1e0592c29ea0b
oliveryoung <oli@oliveryoungdeMacBook-Pro.local>
2018-10-28 10:39:43 +0800

    [A] webpack4 mode

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -2,23 +2,26 @@ const path = require('path')
 const merge = require('webpack-merge')
 const base = require('./webpack.config.base')
 
-module.exports = merge(base, {
-  mode: 'development',
-  entry: path.resolve(__dirname, './src/client/index.js'),
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'public')
-  },
-  module: {
-    rules: [{
-      test: /\.css?$/,
-      use: ['style-loader', {
-        loader: 'css-loader',
-        options: {
-          modules: true,
-          localIdentName: '[name]__[local]--[hash:base64:5]'
-        }
+module.exports = (env, argv) => {
+  const { mode } = argv
+  return merge(base, {
+    mode,
+    entry: path.resolve(__dirname, './src/client/index.js'),
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'public')
+    },
+    module: {
+      rules: [{
+        test: /\.css?$/,
+        use: ['style-loader', {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            localIdentName: '[name]__[local]--[hash:base64:5]'
+          }
+        }]
       }]
-    }]
-  }
-})
+    }
+  })
+}
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -3,25 +3,28 @@ const nodeExternals = require('webpack-node-externals')
 const merge = require('webpack-merge')
 const base = require('./webpack.config.base')
 
-module.exports = merge(base, {
-  target: 'node',
-  mode: 'development',
-  externals: [nodeExternals()],
-  entry: path.resolve(__dirname, './src/server/index.js'),
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
-  },
-  module: {
-    rules: [{
-      test: /\.css?$/,
-      use: ['isomorphic-style-loader', {
-        loader: 'css-loader',
-        options: {
-          modules: true,
-          localIdentName: '[name]__[local]--[hash:base64:5]'
-        }
+module.exports = (env, argv) => {
+  const { mode } = argv
+  return merge(base, {
+    target: 'node',
+    mode,
+    externals: [nodeExternals()],
+    entry: path.resolve(__dirname, './src/server/index.js'),
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'build')
+    },
+    module: {
+      rules: [{
+        test: /\.css?$/,
+        use: ['isomorphic-style-loader', {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            localIdentName: '[name]__[local]--[hash:base64:5]'
+          }
+        }]
       }]
-    }]
-  }
-})
+    }
+  })
+}
