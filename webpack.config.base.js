ee5754e70a75012306d98e397d2518edc1ddb7a4
oliveryoung <oli@oliveryoungdeMacBook-Pro.local>
2018-10-29 10:49:11 +0800

    [A] url-loader

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,16 +1,20 @@
 const path = require('path')
 
 module.exports = {
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    },
+    extensions: ['.js', '.css', '.styl']
+  },
   module: {
     rules: [{
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000'
+    },
+    {
       test: /\.js?$/,
       loader: 'babel-loader',
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, 'src')
-        },
-        extensions: ['.js', '.css', '.styl']
-      },
       exclude: /node_modules/,
       options: {
         presets: ['react', 'stage-0', ['env', {
