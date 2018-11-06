d9b0cabb61d94e2067477163fe8b3c194e9090cb
oliveryoung <oli@oliveryoungdeMacBook-Pro.local>
2018-11-06 11:16:36 +0800

    [A] 关于 页面

--- src/container/About/index.js
+++ src/container/About/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import style from './style.styl'
+
+const info = {
+  name: 'OliverYoung',
+  github: 'https://github.com/oliyg',
+  mail: 'billyangg@qq.com',
+  desc: '该 webApp 基于 React 服务端渲染，NodeJS 为中间层。API 则使用 hapi 开发。前端代码见 github：https://github.com/oliyg/blogSSR ；后台系统 DEMO 请查看项目网址 https://hapiblog.oliyg.com/documentation ；后台系统代码见 github：https://github.com/oliyg/hapiblog'
+}
 
 const About = () => (
-  <div>
-    <div onClick={() => { console.log('d') }}>about</div>
+  <div className={style.container}>
+    <div>{info.name}</div>
+    <div>{info.github}</div>
+    <div>{info.mail}</div>
+    <div>{info.desc}</div>
   </div>
 )
 
